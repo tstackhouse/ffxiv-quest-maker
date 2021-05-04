deeb152b9ad835932f287d7c2506e74c41023a7d
Tim Stackhouse <tstackhouse@gmail.com>
2021-05-04 17:22:09 -0400

    feat: generate quest component
    npx @angular/cli g c quest

---
+++ src/app/quest/quest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quest',
+  templateUrl: './quest.component.html',
+  styleUrls: ['./quest.component.scss']
+})
+export class QuestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
