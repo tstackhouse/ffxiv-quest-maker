a7d220b37ef938fe2e11a75d705356fdeda3475a
Tim Stackhouse <tstackhouse@gmail.com>
2021-05-04 21:21:02 -0400

    feat: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { QuestComponent } from './quest/quest.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { QuestComponent } from './quest/quest.component';
     QuestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
